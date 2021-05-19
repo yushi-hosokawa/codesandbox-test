99bc54231557ed91a46cb2f757043bb75757e1ca
yushi hosokawa <49804320+yushi-hosokawa@users.noreply.github.com>
2021-05-19 16:34:46 +0900

    削除ボタンの作成

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 import "./styles.css";
 
 const onClickAdd = () => {
+  // テキストボックスの値を取得し，初期化する．
   const inputText = document.getElementById("add-text").value;
-  alert(inputText);
+  document.getElementById("add-text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(div)を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("imcomplete-list").removeChild(deleteTarget);
+  });
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  console.log(div);
+
+  //未完了のリストに追加
+  document.getElementById("imcomplete-list").appendChild(div);
 };
 
 document
   .getElementById("add-button")
-  .addEventListener("cleck", () => onClickAdd());
+  .addEventListener("click", () => onClickAdd());
